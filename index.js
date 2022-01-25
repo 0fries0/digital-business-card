78666f15f4a7978c1193ee585293cb5a5d9dcc75
Ritika Tripathi <71340872+0fries0@users.noreply.github.com>
2022-01-25 18:05:39 +0530

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-const emailBtn = document.querySelector(".contact");
-const em = document.querySelector(".em");
-
-emailBtn.addEventListener('click', function(){
-    em.attributes[1].textContent = "mailto: retecatripathi@gmail.com";
-})
